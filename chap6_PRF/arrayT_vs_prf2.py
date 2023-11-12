5817753d03aa66ace23251910af382c3910dde2d
Hacker-Code-J <hacker3740@gmail.com>
2023-11-12 16:46:56 +0900

    Desktop231112v2

--- chap6_PRF/arrayT_vs_prf2.py
+++ chap6_PRF/arrayT_vs_prf2.py
@@ -11,15 +11,18 @@ output_space = range(2**8)  # Let's assume the output space is 8-bit values
 
 # Example PRF using a simple hash function (not cryptographically secure)
 def example_prf(key, input_value):
-    return (hash(f"{key}-{input_value}") % (2**8))
+    return (hash(f"{key}^{input_value}") % (2**8))
 
 # Example RF that simply chooses a random value from the output space for every input
 def example_rf(input_value):
     return np.random.choice(output_space)
 
 # Generate outputs for PRFs with two different keys and RF with a larger sample size
-large_prf_outputs_key1 = [example_prf(1, x) for x in input_space_large]
-large_prf_outputs_key2 = [example_prf(3, x) for x in input_space_large]
+key1 = np.random.choice(key_space)
+key2 = np.random.choice(key_space)
+
+large_prf_outputs_key1 = [example_prf(key1, x) for x in input_space_large]
+large_prf_outputs_key2 = [example_prf(key2, x) for x in input_space_large]
 large_rf_outputs = [example_rf(x) for x in input_space_large]
 
 # Convert the input space from range to list for correct repetition
@@ -27,7 +30,7 @@ input_space_list = list(input_space_large)
 
 # Corrected data dictionary
 data = {
-    'Input': input_space_list * 3,  # Repeat the input space for each label
+    'Index': input_space_list * 3,  # Repeat the input space for each label
     'Output': large_prf_outputs_key1 + large_prf_outputs_key2 + large_rf_outputs,
     'Label': (['PRF Key 1'] * num_samples_large) + 
              (['PRF Key 2'] * num_samples_large) + 
@@ -41,7 +44,7 @@ df = pd.DataFrame(data)
 g = sns.FacetGrid(df, col="Label", col_wrap=3, height=4, sharex=True, sharey=True)
 
 # Map a density plot to each subset
-g.map(sns.kdeplot, "Input", "Output", cmap="viridis", shade=True, bw_adjust=0.5)
+g.map(sns.kdeplot, "Index", "Output", cmap="viridis", shade=True, bw_adjust=0.5)
 
 # Show the plot
 plt.show()
---
+++ chap6_PRF/arrayT_vs_prf4.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Let's visualize the concept of a Pseudo-Random Function (PRF) using a simple example.
+# We'll generate a random key and use a simple hash function as an example PRF.
+
+# Generate a random key
+key1 = np.random.randint(0, 2**10, size=1)  # A 1-byte key
+key2 = np.random.randint(0, 2**10, size=1)  # A 8-byte key
+
+# Define a simple PRF using a hash function
+# For visualization purposes, we'll just use a hash of the input x XORed with the key
+def prf(x, k):
+    # XOR the input with the key
+    x_k = np.bitwise_xor(x, k)
+    # Hash the result to get the PRF output
+    return x_k # % (2**10)
+    # return np.mod(np.sum(x_k), 256)  # Output space is Y = {0, 1, ..., 255}
+
+# Define a truly random function (RF)
+def rf(x, y_space):
+    # Choose a random output from the output space for each input
+    return np.random.choice(y_space)
+
+# Let's apply different sizes for the input and output space to both the PRF and RF and visualize the results.
+
+# Define new input and output spaces of different sizes
+input_space1 = np.arange(2**10)
+output_space = np.arange(2**10)
+
+# Compute the PRF and RF outputs for the smaller input/output space
+prf_output1 = np.array([prf(x, key1) for x in input_space1])
+rf_output1 = np.array([rf(x, output_space) for x in input_space1])
+
+prf_output2 = np.array([prf(x, key2) for x in input_space1])
+rf_output2 = np.array([rf(x, output_space) for x in input_space1])
+
+# Visualization
+plt.figure(figsize=(14, 7))
+
+plt.subplot(1, 2, 1)
+plt.plot(input_space1, prf_output1, 'o', label='PRF')
+plt.plot(input_space1, rf_output1, 'x', label='RF')
+plt.title('PRF vs. RF (Small Input/Output Space)')
+plt.xlabel('Input Space X')
+plt.ylabel('Output Space Y')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(1, 2, 2)
+plt.plot(input_space1, prf_output2, 'o', label='PRF')
+plt.plot(input_space1, rf_output2, 'x', label='RF')
+plt.title('PRF vs. RF (Small Input/Output Space)')
+plt.xlabel('Input Space X')
+plt.ylabel('Output Space Y')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+
+# # Input space X = {0, 1, ..., 255}
+# input_space = np.arange(256)
+
+# # Compute the PRF output for each input in the input space
+# prf_output = np.array([prf(x, key) for x in input_space])
+
+# # Output space Y for the RF is the same as the PRF for comparison
+# y_space = np.arange(256)
+
+# # Compute the RF output for each input in the input space
+# rf_output = np.array([rf(x, y_space) for x in input_space])
+
+# # Plot the PRF and RF together for comparison
+# plt.figure(figsize=(12, 6))
+
+# # PRF plot
+# plt.plot(input_space, prf_output, 'o', label='PRF')
+
+# # RF plot
+# plt.plot(input_space, rf_output, 'x', label='RF')
+
+# plt.title('Pseudo-Random Function (PRF) vs. Truly Random Function (RF) Visualization')
+# plt.xlabel('Input Space X')
+# plt.ylabel('Output Space Y')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
